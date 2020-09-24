0f7612c93c47d1a8047e659a25bc701528c7b401
Noah Day <sheltielover432@gmail.com>
2020-09-23 21:21:07 -0400

    Circle Class

---
+++ ArcadeApplication/ArcadeApplication/Circle.cpp
@@ -0,0 +1,21 @@
+#include "Circle.h"
+#include "Utils.h"
+
+Circle::Circle() : Circle(Vec2D::Zero, 0)
+{
+}
+
+Circle::Circle(const Vec2D& center, float radius)
+{
+	mPoints.push_back(center);
+}
+
+bool Circle::Intersects(const Circle& otherCircle) const
+{
+	return GetCenterPoint().Distance(otherCircle.GetCenterPoint()) < (mRadius + otherCircle.mRadius);
+}
+
+bool Circle::ContainsPoint(const Vec2D& point) const
+{
+	return IsLessThanOrEqual(GetCenterPoint().Distance(point), mRadius);
+}
